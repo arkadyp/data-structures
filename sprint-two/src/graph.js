4a1973642d54b3c807bb1ca70874facac16772ea
arkadyp <arkady.pevzner@gmail.com>
2014-01-12 21:24:11 -0800

    Refactors graph to change name of returnName to getName

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -10,16 +10,16 @@ var Graph = function(){
 
 Graph.prototype.addNode = function(newNode, toNode){
   var node = new Node(newNode);
-  var nodeName = this.returnName(newNode);
+  var nodeName = this.getName(newNode);
   this.nodes[nodeName] = node;
   
   if(!(toNode === undefined)) { //if toNode is passed in, connect it to new node
-    var toNodeName = this.returnName(toNode);
+    var toNodeName = this.getName(toNode);
     this.addEdge(nodeName, toNodeName);
   } else if(this.nodeCount === 1) {  //automatically create an edge if there is only one node in graph
     var graph = this;
     _.each(this.nodes, function(firstNode){ //only one node is in graph at this point
-      graph.addEdge(nodeName, graph.returnName(firstNode.value));
+      grapha.addEdge(nodeName, graph.getName(firstNode.value));
     });
   }
 
@@ -27,11 +27,11 @@ Graph.prototype.addNode = function(newNode, toNode){
 };
 
 Graph.prototype.contains = function(target){
-  return (this.returnName(target) in this.nodes);
+  return (this.getName(target) in this.nodes);
 };
 
 Graph.prototype.removeNode = function(target){
-  var nodeName = this.returnName(target);
+  var nodeName = this.getName(target);
   if(nodeName in this.nodes) {
     delete this.nodes[nodeName];
   }
@@ -41,8 +41,8 @@ Graph.prototype.getEdge = function(fromNode, toNode){
 };
 
 Graph.prototype.addEdge = function(target1, target2){
-  var node1Name = this.returnName(target1);
-  var node2Name = this.returnName(target2);
+  var node1Name = this.getName(target1);
+  var node2Name = this.getName(target2);
   var node1 = this.nodes[node1Name];
   var node2 = this.nodes[node2Name];
   
@@ -55,6 +55,6 @@ Graph.prototype.addEdge = function(target1, target2){
 Graph.prototype.removeEdge = function(fromNode, toNode){
 };
 
-Graph.prototype.returnName = function(name){
+Graph.prototype.getName = function(name){
   return JSON.stringify(name);
 };
